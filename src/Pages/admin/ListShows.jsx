d5704dc224fd0c7521d46e41de5dba020427f6a6
Ayush Adhikari <ayushadhikari080@gmail.com>
2026-06-12 18:07:09 +0530

    add List-Shows Page

--- src/Pages/admin/ListShows.jsx
+++ src/Pages/admin/ListShows.jsx
@@ -1,9 +1,78 @@
-
+import { useEffect, useState } from "react";
+import { dummyShowsData } from "../../assets/assets";
+import Loading from "../../Components/Loading";
+import Title from "../../Components/admin/Title";
+import { dateFormat } from "../../Lib/dataFormat";
 
 const ListShows = () => {
-  return (
-    <div>ListShows</div>
-  )
-}
+  const currency = import.meta.env.VITE_CURRENCY;
 
-export default ListShows
+  const [show, setShow] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getAllShows = async () => {
+    try {
+      setShow([
+        {
+          movie: dummyShowsData[0],
+          showDataTime: "2025-06-30T02:30:00.000Z",
+          showPrice: 59,
+          occupiedSeats: {
+            A1: "user_1",
+            B1: "user_2",
+            C1: "user_3",
+          },
+        },
+      ]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(dummyShowsData[0]);
+
+  useEffect(() => {
+    getAllShows();
+  }, []);
+
+  return !loading ? (
+    <>
+      <Title text1="List" text2="Shows" />
+      <div className="max-w-4xl mt-6 overflow-x-auto">
+        <table className="w-full mt-6 ">
+          <thead>
+            <tr className="bg-primary/20 text-left text-white">
+              <th className="p-2 font-medium pl-5">Movie Name</th>
+              <th className="p-2 font-medium">Show Time</th>
+              <th className="p-2 font-medium">Total Bookings</th>
+              <th className="p-2 font-medium">Earnings</th>
+            </tr>
+          </thead>
+          <tbody className="text-sm font-light">
+            {show.map((shows, index) => (
+              <tr
+                key={index}
+                className="border-b border-primary/10 bg-primary/5 even:bg-primary/10"
+              >
+                <td className="p-2 min-w-45 pl-5">{shows.movie.title}</td>
+                <td className="p-2">{dateFormat(shows.showDataTime)}</td>
+                <td className="p-2">
+                  {Object.keys(shows.occupiedSeats).length}
+                </td>
+                <td className="p-2">
+                  {currency}{" "}
+                  {Object.keys(shows.occupiedSeats).length * shows.showPrice}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default ListShows;
